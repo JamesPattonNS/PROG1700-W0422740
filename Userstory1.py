e6a46de50116d4a00c3b99fdf4c39c36b7704ce8
Patton <W0422740@campus.nscc.ca>
2018-09-18 10:21:37 -0300

    Somewhat Completed, Base set up, taxes not.

---
+++ Userstory1.py
@@ -0,0 +1,20 @@
+# Prog1700-Student15
+"""
+Hello World Project
+
+First In Class Work
+
+Name...: James, Patton
+ID...: W0422740
+"""
+__AUTHOR__ = "James Patton <w0422740@nscc.ca>"
+
+#CupCost
+costOfCup = 1.25
+
+print("Hello.")
+Cups = input("How many cups today: ")
+numCups = int(Cups)
+Cost = float(numCups * costOfCup)
+Result = str(Cost)
+print("That will be "+Result+"")
