0d46f4d6a717cd1926bbeb14d96aca6e44e6f607
Patton <W0422740@campus.nscc.ca>
2018-09-18 09:05:14 -0300

    :)

---
+++ Practice.py
@@ -0,0 +1,17 @@
+# Prog1700-Student15
+"""
+Hello World Project
+
+First In Class Work
+
+Name...: James, Patton
+ID...: W0422740
+"""
+__AUTHOR__ = "James Patton <w0422740@nscc.ca>"
+
+name = input("What is your name: ")
+print("Hello " + name + ", pleased to meet you.")
+strAge = input("How old are you: ")
+numAge = float(strAge)
+timeToRetire = str(65 - numAge)
+print("You have " + timeToRetire + " years before retirement.")
