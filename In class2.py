911e0b560d3ff55a710c0cc82c5914039cb04a57
Patton <W0422740@campus.nscc.ca>
2018-09-19 12:27:30 -0300

    :)

---
+++ In class2.py
@@ -0,0 +1,12 @@
+    #variables
+fname = "James"
+lname = "Patton"
+age =  26.39 
+phrase = "First: {0} \n Last: {1} \n age:{2:.2f} "
+
+    #Execution Commands
+phrase= phrase.format (fname, lname, age)
+
+
+
+print(phrase)
--- Userstory1.py
+++ Userstory1.py
@@ -11,10 +11,18 @@ __AUTHOR__ = "James Patton <w0422740@nscc.ca>"
 
 #CupCost
 costOfCup = 1.25
+hsttaxes = 0.15
 
+#Part1
 print("Hello.")
 Cups = input("How many cups today: ")
 numCups = int(Cups)
 Cost = float(numCups * costOfCup)
 Result = str(Cost)
-print("That will be "+Result+"")
+print("That will be "+Result+" before taxes ")
+
+#Part2
+tax = float(Result * hsttaxes)
+
+
+print("Your tax will be " +tax+"")
